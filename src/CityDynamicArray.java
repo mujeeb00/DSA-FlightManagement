aad4eee2958b56209b5f3e11cdaf41ff4cf99421
mujeeb00 <mujeebfx160@gmail.com>
2024-12-29 21:09:32 +0500

    fix : implement custom array copy

--- src/CityDynamicArray.java
+++ src/CityDynamicArray.java
@@ -22,7 +22,9 @@ public class CityDynamicArray {
     private void resize() {
         int newCapacity = data.length * 2;
         City[] newData = new City[newCapacity];
-        System.arraycopy(data, 0, newData, 0, size);
+        for (int i = 0; i < size; i++) {
+            newData[i] = data[i];
+        }
         data = newData;
     }
 
